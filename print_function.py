8cf389a8ecb6cb339b433f50e2b5b8470709e896
zjynb2659558059 <2659558059@qq.com>
2022-05-08 20:57:31 +0800

    modified print

--- print_function.py
+++ print_function.py
@@ -5,7 +5,9 @@ print(520)
 print('\n')
 print(4+6)
 print('\n')
-#将数据输出到文件当中
-fp=open('G:/Git/gitcode/Python-Learning/hello2.txt','a+')
+#将数据输出到文件当中 ,注意点。1，所指定的盘符存在， 2，使用file=fp
+fp=open('G:/Git/gitcode/Python-Learning/hello2.txt','a+') #如果文件不存在就创建，存在即在文件内容的后面继续追加（a+)
 print('hello world!',file=fp)
 fp.close()
+#不进行换行输出，输出内容在一行当中
+print('hello','world','Python')
