aba0aeb33a474338c6818d168f9bd36159722934
zjynb2659558059 <2659558059@qq.com>
2022-05-09 13:59:41 +0800

    数据类型以及数据类型转换

--- 数据类型以及数据类型转换.py
+++ 数据类型以及数据类型转换.py
@@ -40,4 +40,39 @@ print(str3,type(str3))
 print(str4,type(str4))
 
 #类型转换_str()函数与int()函数，float()函数
+name='张三'
+age=20
+
+print(type(name),type(age)) #说明name与age的数据类型不相同
+#print('我叫'+name+'今年'+age) #TypeError: 'module' object is not callable 只能连接相同类型，所以需要数据类型转换
+print('我叫'+name+'今年'+str(age))
+print('---------str()将其他类型转换成str类型----------') #str()也可用引号转换
+a=10;b=198.8;c=False
+print(type(a),type(b),type(c))
+print(str(a),str(b),str(c),type(str(a)),type(str(b)),type(str(c)))
+print('10',type('10'))
+
+#int()将其他类型转换成int类型
+s1='126';f1=98.7;s2='76.77';ff=True;s3='hello'
+print(type(s1),type(f1),type(s2),type(ff),type(3))
+print(int(s1),type(int(s1))) #将str转换成int类型,字符串为数字串
+print(int(f1),type(int(f1))) #将float转换成int类型，截取整数部分，舍掉小数部分
+#print(int(s2),type(int(s2))) #将str转换成int类型，字符串为小数串,报错，因为字符串为小数串
+print(int(ff),type(int(ff))) #将bool转换成int类型，True为1，False为0
+#print(int(s3),type(int(s3))) #报错，将str转换成int类型时，字符串必须为数字串（整数),非数字串是不允许转换
+
+#float()将其他类型转换成float类型 1，文字类无法转换成整数 2，整数转换成浮点数，末尾为.0
+print('--------------float()，将其他数据类型转换成float类型')
+
+s1='128.98';s2='76';ff=True;s3='Hello';i=98
+print(float(s1),type(float(s1)))
+print(float(s2),type(float(s2)))
+print(float(ff),type(float(ff)))
+#print(float(s3),type(float(s3))) #报错，文字类无法转换成整数
+print(float(i),type(float(i)))
+
+
+
+
+
 
